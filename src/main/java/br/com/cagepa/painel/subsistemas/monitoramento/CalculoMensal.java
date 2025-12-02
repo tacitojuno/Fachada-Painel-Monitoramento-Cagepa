46398490ddc76392ed3188cf0d85b52943fed2dd
Tácito <magoender@gmail.com>
2025-12-02 20:36:30 -0300

    Implementação de Lógica Simples.

--- src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/CalculoMensal.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/CalculoMensal.java
@@ -1,4 +1,17 @@
 package br.com.cagepa.painel.subsistemas.monitoramento;
 
-public class CalculoMensal {
+import br.com.cagepa.painel.core.entidades.Leitura;
+import br.com.cagepa.painel.infra.Logger;
+import java.util.List;
+
+public class CalculoMensal implements ICalculoConsumoStrategy {
+
+    @Override
+    public double calcular(List<Leitura> leituras) {
+        Logger.getInstance().log("STRATEGY: Calculando média de consumo mensal...");
+        if(leituras.isEmpty()) return 0.0;
+
+        double soma = leituras.stream().mapToDouble(Leitura::getValorM3).sum();
+        return soma / leituras.size();
+    }
 }
