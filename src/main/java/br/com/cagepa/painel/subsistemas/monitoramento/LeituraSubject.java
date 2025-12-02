c7c7a1046695aa8bffaa61c49d2ad6f6959ed0e9
Tácito <magoender@gmail.com>
2025-12-02 20:37:54 -0300

    Gerencia lista de observadores e dispara notificações.

--- src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/LeituraSubject.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/LeituraSubject.java
@@ -1,4 +1,21 @@
 package br.com.cagepa.painel.subsistemas.monitoramento;
 
+import br.com.cagepa.painel.core.entidades.Leitura;
+import br.com.cagepa.painel.infra.Logger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeituraSubject {
+    private final List<IMonitoramentoObserver> observadores = new ArrayList<>();
+
+    public void adicionarObservador(IMonitoramentoObserver obs) {
+        observadores.add(obs);
+    }
+
+    public void notificarObservadores(Leitura leitura) {
+        Logger.getInstance().log("OBSERVER: Notificando " + observadores.size() + " sistemas interessados sobre nova leitura.");
+        for (IMonitoramentoObserver obs : observadores) {
+            obs.notificarNovaLeitura(leitura);
+        }
+    }
 }
