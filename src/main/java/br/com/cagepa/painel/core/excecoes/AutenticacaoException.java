5d07314a34da97a5773e43abfd4fa181b5114e5c
Tácito <magoender@gmail.com>
2025-12-01 23:36:48 -0300

    Criação de Exceção

--- src/main/java/br/com/cagepa/painel/core/excecoes/AutenticacaoException.java
+++ src/main/java/br/com/cagepa/painel/core/excecoes/AutenticacaoException.java
@@ -1,4 +1,7 @@
 package br.com.cagepa.painel.core.excecoes;
 
-public class AutenticacaoException {
+public class AutenticacaoException extends RuntimeException{
+    public AutenticacaoException(String message) {
+        super(message);
+    }
 }
