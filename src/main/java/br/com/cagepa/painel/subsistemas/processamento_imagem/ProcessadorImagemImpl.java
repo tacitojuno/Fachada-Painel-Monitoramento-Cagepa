c7609c2671ef5551b3efacee8c5d0dfeb969cd37
Tácito <magoender@gmail.com>
2025-12-02 20:35:31 -0300

    Estende o Template e usa o Adapater.

--- src/main/java/br/com/cagepa/painel/subsistemas/processamento_imagem/ProcessadorImagemImpl.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/processamento_imagem/ProcessadorImagemImpl.java
@@ -1,4 +1,17 @@
 package br.com.cagepa.painel.subsistemas.processamento_imagem;
 
-public class ProcessadorImagemImpl {
+import java.io.File;
+
+public class ProcessadorImagemImpl extends ImagemProcessorTemplate {
+
+    private final OCRAdapter ocrAdapter;
+
+    public ProcessadorImagemImpl() {
+        this.ocrAdapter = new TesseractAdapter();
+    }
+
+    @Override
+    protected String aplicarOCR(File imagem) {
+        return ocrAdapter.extrairTexto(imagem);
+    }
 }
