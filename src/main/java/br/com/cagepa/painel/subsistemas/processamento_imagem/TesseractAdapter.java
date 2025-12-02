088c4cecaf236793b836f9d78b83b240e3f3e1d8
Tácito <magoender@gmail.com>
2025-12-02 20:32:29 -0300

    Simulação de Tesseract OCR com Adapter.

--- src/main/java/br/com/cagepa/painel/subsistemas/processamento_imagem/TesseractAdapter.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/processamento_imagem/TesseractAdapter.java
@@ -1,4 +1,19 @@
 package br.com.cagepa.painel.subsistemas.processamento_imagem;
 
-public class TesseractAdapter {
+import br.com.cagepa.painel.infra.Logger;
+import java.io.File;
+import java.util.Random;
+
+//Adapter para o Tesseract OCR
+public class TesseractAdapter implements OCRAdapter {
+
+    @Override
+    public String extrairTexto(File imagem) {
+        Logger.getInstance().log("ADAPTER: Solicitando leitura de OCR para o arquivo " + imagem.getName());
+
+        //SIMULAÇÃO: Retorna um número aleatório como se tivesse lido a imagem
+        double leituraSimulada = 100 + new Random().nextDouble() * 50;
+
+        return String.format("%.2f", leituraSimulada).replace(",", ".");
+    }
 }
