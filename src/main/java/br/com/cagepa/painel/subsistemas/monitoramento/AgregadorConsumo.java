ded413229967de0603dc992eefb0c42ae78226f2
Tácito <magoender@gmail.com>
2025-12-02 20:32:10 -0300

    Utilização de Strategy e Observer.

--- src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/AgregadorConsumo.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/AgregadorConsumo.java
@@ -1,4 +1,34 @@
 package br.com.cagepa.painel.subsistemas.monitoramento;
 
+import br.com.cagepa.painel.core.entidades.Leitura;
+import java.util.List;
+
 public class AgregadorConsumo {
+
+    //Composição com Strategy
+    private ICalculoConsumoStrategy strategy;
+    //Composição com o Subject
+    private LeituraSubject subject;
+
+    public AgregadorConsumo() {
+        this.subject = new LeituraSubject();
+        this.strategy = new CalculoMensal();
+    }
+
+    public void setStrategy(ICalculoConsumoStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public LeituraSubject getSubject() {
+        return subject;
+    }
+
+    public double processarHistorico(List<Leitura> historico) {
+        return strategy.calcular(historico);
+    }
+
+    public void novaLeituraRecebida(Leitura leitura) {
+        //... Lógica de salvar no vanco de dados, etc.
+        subject.notificarObservadores(leitura);
+    }
 }
