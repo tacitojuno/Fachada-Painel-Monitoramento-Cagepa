b508c988d02134422882bdf8be13f38b4de3aadc
Tácito <magoender@gmail.com>
2025-12-01 23:36:18 -0300

    Simulação de Token

--- src/main/java/br/com/cagepa/painel/subsistemas/auth/JWTToken.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/auth/JWTToken.java
@@ -1,4 +1,16 @@
 package br.com.cagepa.painel.subsistemas.auth;
 
+//Simulação simples de um token
 public class JWTToken {
+    private String tokenString;
+    private String usuarioEmail;
+
+    public JWTToken(String tokenString, String usuarioEmail) {
+        this.tokenString = tokenString;
+        this.usuarioEmail = usuarioEmail;
+    }
+
+    public String getTokenString() {
+        return tokenString;
+    }
 }
