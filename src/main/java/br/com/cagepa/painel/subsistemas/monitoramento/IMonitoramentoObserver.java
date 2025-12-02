022414850d884fad9bcc6369bb55b8613b4c3f16
Tácito <magoender@gmail.com>
2025-12-02 20:32:53 -0300

    Interface do Observer.

--- src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/IMonitoramentoObserver.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/IMonitoramentoObserver.java
@@ -1,4 +1,8 @@
 package br.com.cagepa.painel.subsistemas.monitoramento;
 
+import br.com.cagepa.painel.core.entidades.Leitura;
+
+//Observer
 public interface IMonitoramentoObserver {
+    void notificarNovaLeitura(Leitura leitura);
 }
