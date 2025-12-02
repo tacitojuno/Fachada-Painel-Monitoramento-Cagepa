f5968523b19b3fb8bb43fa3bc3772735af3f720d
Tácito <magoender@gmail.com>
2025-12-01 23:30:54 -0300

    Implementação Repository (Interface)

--- src/main/java/br/com/cagepa/painel/subsistemas/clientes/ClienteRepository.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/clientes/ClienteRepository.java
@@ -1,4 +1,13 @@
 package br.com.cagepa.painel.subsistemas.clientes;
 
+import br.com.cagepa.painel.core.entidades.Cliente;
+import java.util.List;
+import java.util.Optional;
+
+//Padrão Repository (Interface)
 public interface ClienteRepository {
+    void salvar(Cliente cliente);
+    Optional<Cliente> buscarPorCpf(String cpf);
+    List<Cliente> listarTodos();
+    void deletar(String cpf);
 }
