df1ad5aa7f617ea268c605f00c3d9bad65f3bdf7
Tácito <magoender@gmail.com>
2025-12-02 20:33:04 -0300

    Interface do Strategy (Cálculo).

--- src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/ICalculoConsumoStrategy.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/monitoramento/ICalculoConsumoStrategy.java
@@ -1,4 +1,9 @@
 package br.com.cagepa.painel.subsistemas.monitoramento;
 
+import br.com.cagepa.painel.core.entidades.Leitura;
+import java.util.List;
+
+//Strategy
 public interface ICalculoConsumoStrategy {
+    double calcular(List<Leitura> leituras);
 }
