78f7cf18bd540a65487c89323f81453d399afc3f
Tácito <magoender@gmail.com>
2025-12-01 23:29:48 -0300

    Implementação Singleton

--- src/main/java/br/com/cagepa/painel/infra/Logger.java
+++ src/main/java/br/com/cagepa/painel/infra/Logger.java
@@ -1,4 +1,25 @@
 package br.com.cagepa.painel.infra;
 
+import java.time.LocalDateTime;
+
 public class Logger {
+    //Padrão Singleton
+    private static Logger instancia;
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if (instancia == null) {
+            instancia = new Logger();
+        }
+        return instancia;
+    }
+
+    public void log(String mensagem) {
+        System.out.println("[LOG " + LocalDateTime.now() + "] " + mensagem);
+    }
+
+    public void logErro(String mensagem) {
+        System.err.println("[ERRO " + LocalDateTime.now() + "] " + mensagem);
+    }
 }
--- src/main/java/br/com/cagepa/painel/subsistemas/auth/AuthManager.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/auth/AuthManager.java
@@ -1,4 +1,42 @@
 package br.com.cagepa.painel.subsistemas.auth;
 
+import br.com.cagepa.painel.core.excecoes.AutenticacaoException;
+import br.com.cagepa.painel.infra.Logger;
+
+import java.util.UUID;
+
 public class AuthManager {
+    //Singleton
+    private static AuthManager instance;
+    private String tokenValidoAtual = null; //Armazena o token ativo para teste
+
+    private AuthManager() {}
+
+    public static AuthManager getInstance() {
+        if (instance == null) {
+            instance = new AuthManager();
+        }
+        return instance;
+    }
+
+    public JWTToken login(String email, String senha) {
+        //Simula validação
+        if ("admin@cagepa.com.br".equals(email) && "admin123".equals(senha)) {
+            String tokenHash = UUID.randomUUID().toString();
+            this.tokenValidoAtual = tokenHash;
+            Logger.getInstance().log("Login realizado com sucesso: " + email);
+            return new JWTToken(tokenHash, email);
+        }
+        Logger.getInstance().logErro("Login invalido: " + email);
+        throw new AutenticacaoException("Credenciais invalidas");
+    }
+
+    public boolean validarToken(String token) {
+        return token != null && token.equals(this.tokenValidoAtual);
+    }
+
+    public void logout() {
+        this.tokenValidoAtual = null;
+        Logger.getInstance().log("Logout realizado com sucesso.");
+    }
 }
