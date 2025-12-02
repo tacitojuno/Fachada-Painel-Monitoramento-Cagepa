22179b334a65853c5158bcbbaf2615f90ce2015b
Tácito <magoender@gmail.com>
2025-12-01 23:31:10 -0300

    Implementação Factory

--- src/main/java/br/com/cagepa/painel/subsistemas/clientes/ClienteFactory.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/clientes/ClienteFactory.java
@@ -1,4 +1,14 @@
 package br.com.cagepa.painel.subsistemas.clientes;
 
+import br.com.cagepa.painel.core.entidades.Cliente;
+
+//Padrão Factory: Classe responsável por criar instâncias de Cliente.
 public class ClienteFactory {
+
+    public Cliente criarCliente(String nome, String cpfCnpj, String email) {
+        if(cpfCnpj == null || cpfCnpj.length() != 11) {
+            throw new IllegalArgumentException("CPF/CNPJ inválido.");
+        }
+        return new Cliente(cpfCnpj, nome, email);
+    }
 }
