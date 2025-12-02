088a5101913ed49e0e332936ffd7c39bf0fd179b
Tácito <magoender@gmail.com>
2025-12-01 23:35:50 -0300

    Definição de contrato para Proxy e Fachada Real

--- src/main/java/br/com/cagepa/painel/core/fachada/IFachada.java
+++ src/main/java/br/com/cagepa/painel/core/fachada/IFachada.java
@@ -1,4 +1,13 @@
 package br.com.cagepa.painel.core.fachada;
 
+import br.com.cagepa.painel.core.dtos.ClienteDTO;
+import br.com.cagepa.painel.subsistemas.auth.JWTToken;
+
+import java.util.List;
+
+//Definne o contrato da Fachada e do Proxy
 public interface IFachada {
+    JWTToken login(String email, String senha);
+    void cadastrarCliente(String nome, String cpf, String token);
+    List<ClienteDTO> listarClientes(String token);
 }
