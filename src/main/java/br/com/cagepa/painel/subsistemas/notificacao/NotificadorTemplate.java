923a0028a29a01b7bb2e27091637f7cee09ec876
Tácito <magoender@gmail.com>
2025-12-03 12:35:38 -0300

    Padronização de notificação.

--- src/main/java/br/com/cagepa/painel/subsistemas/notificacao/NotificadorTemplate.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/notificacao/NotificadorTemplate.java
@@ -1,4 +1,23 @@
 package br.com.cagepa.painel.subsistemas.notificacao;
 
-public class NotificadorTemplate {
+import br.com.cagepa.painel.infra.Logger;
+
+public abstract class NotificadorTemplate {
+
+    public final void enviarNotificacao(String destinatario, String mensagem) {
+        if(validarDestinatario(destinatario)) {
+            String msgFormatada = formatarMensagem(mensagem);
+            realizarEnvio(destinatario, msgFormatada);
+        } else {
+            Logger.getInstance().logErro("Destinatário inválido: " + destinatario);
+        }
+    }
+
+    protected abstract String formatarMensagem(String crua);
+    protected abstract void realizarEnvio(String dest, String msg);
+
+    //Opcional: validação padrão do destinatário
+    protected boolean validarDestinatario(String dest) {
+        return dest != null && !dest.isEmpty();
+    }
 }
