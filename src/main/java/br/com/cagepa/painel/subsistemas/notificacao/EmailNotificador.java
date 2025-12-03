b83f18abc5b006ec5a4372b3729571822c9ac470
Tácito <magoender@gmail.com>
2025-12-03 12:36:43 -0300

    Logger que simula envio de notificação.

--- src/main/java/br/com/cagepa/painel/subsistemas/notificacao/EmailNotificador.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/notificacao/EmailNotificador.java
@@ -1,4 +1,16 @@
 package br.com.cagepa.painel.subsistemas.notificacao;
 
-public class EmailNotificador {
+import br.com.cagepa.painel.infra.Logger;
+
+public class EmailNotificador extends NotificadorTemplate {
+
+    @Override
+    protected String formatarMensagem(String crua) {
+        return "ASSUNTO: Alerta Cagepa\nCORPO: " + crua + "\nRodapé: Não responda.";
+    }
+
+    @Override
+    protected void realizarEnvio(String dest, String msg) {
+        Logger.getInstance().log("NOTIFICACAO (Email) enviado para " + dest + ":\n" + msg);
+    }
 }
