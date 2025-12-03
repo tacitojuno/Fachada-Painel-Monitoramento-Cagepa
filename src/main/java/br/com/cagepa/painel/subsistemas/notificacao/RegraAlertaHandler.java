60f8630eb9813d022fe23bb60eb04ca879d66dfe
Tácito <magoender@gmail.com>
2025-12-03 12:33:24 -0300

    Implementação de Chain of Responsability.

--- src/main/java/br/com/cagepa/painel/subsistemas/notificacao/RegraAlertaHandler.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/notificacao/RegraAlertaHandler.java
@@ -1,4 +1,23 @@
 package br.com.cagepa.painel.subsistemas.notificacao;
 
-public class RegraAlertaHandler {
+import br.com.cagepa.painel.core.entidades.Leitura;
+
+public abstract class RegraAlertaHandler {
+
+    protected RegraAlertaHandler proximo;
+
+    //Define próximo passo da corrente
+    public void setProximo(RegraAlertaHandler proximo) {
+        this.proximo = proximo;
+    }
+
+    //Processa e passa adiante
+    public abstract void verificar(Leitura leitura, double limiteMensal);
+
+    //Passar para o próximo se ele existir
+    protected void chamarProximo(Leitura leitura, double limite) {
+        if (proximo != null) {
+            proximo.verificar(leitura, limite);
+        }
+    }
 }
