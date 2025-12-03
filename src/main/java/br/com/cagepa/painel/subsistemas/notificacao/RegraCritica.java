ce76cc5c89594175081f8754a1d04d69ad5b8d5a
Tácito <magoender@gmail.com>
2025-12-03 12:34:43 -0300

    Definição de regras concretas.

--- src/main/java/br/com/cagepa/painel/subsistemas/notificacao/RegraCritica.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/notificacao/RegraCritica.java
@@ -1,4 +1,15 @@
 package br.com.cagepa.painel.subsistemas.notificacao;
 
-public class RegraCritica {
+import br.com.cagepa.painel.core.entidades.Leitura;
+import br.com.cagepa.painel.infra.Logger;
+
+public class RegraCritica extends RegraAlertaHandler {
+
+    @Override
+    public void verificar(Leitura leitura, double limiteMensal) {
+        if(leitura.getValorM3() >= limiteMensal) {
+            Logger.getInstance().logErro("CHAIN: [ALERTA CRÍTICO] Limite ultrapassado! " + leitura.getValorM3() + "m3 > " + limiteMensal);
+        }
+        chamarProximo(leitura, limiteMensal);
+    }
 }
--- src/main/java/br/com/cagepa/painel/subsistemas/notificacao/RegraPreventiva.java
+++ src/main/java/br/com/cagepa/painel/subsistemas/notificacao/RegraPreventiva.java
@@ -1,4 +1,16 @@
 package br.com.cagepa.painel.subsistemas.notificacao;
 
-public class RegraPreventiva {
+import br.com.cagepa.painel.core.entidades.Leitura;
+import br.com.cagepa.painel.infra.Logger;
+
+public class RegraPreventiva  extends RegraAlertaHandler {
+
+    @Override
+    public void verificar(Leitura leitura, double limiteMensal) {
+        if(leitura.getValorM3() >= (limiteMensal * 0.8) && leitura.getValorM3() < limiteMensal){
+            Logger.getInstance().log("CHAIN: [Alerta Preventivo] Consumo atingiu 80% do limite (" + leitura.getValorM3() + "m3)");
+            //Aqui chamaria o notificador, ex: email, sms, push, etc.
+        }
+        chamarProximo(leitura, limiteMensal);
+    }
 }
