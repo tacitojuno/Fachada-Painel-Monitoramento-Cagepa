263c76d8cd61093ba2518a5794a47f324738af72
Tácito <magoender@gmail.com>
2025-12-04 15:56:00 -0300

    build: Atualização de dependências para Tesseract OCR.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.sourceforge.tess4j:tess4j:5.9.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks.test {
